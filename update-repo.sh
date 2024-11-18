36b67b45af933dba05b82d3a102870b177815a3b
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-11-18 21:42:12 +0100

    chore: adjust

--- update-repo.sh
+++ update-repo.sh
@@ -19,7 +19,8 @@ update_repo() {
     cd "$REPO_DIR" || exit
     rm -f Packages* Contents-iphoneos-arm* Release* 2> /dev/null
 
-    /usr/bin/apt-ftparchive packages --db /tmp/apt-ftparchive-db debs > Packages
+    /usr/bin/apt-ftparchive packages debs > Packages
+
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
